f751fe546728c3e7924073d71b830cb15d2aa513
Nathan Stchepinsky <nathanstchepinsky@protonmail.com>
2021-11-10 12:41:20 +0100

    kernel panic

--- iLocked/ViewController/SelectIcon.swift
+++ iLocked/ViewController/SelectIcon.swift
@@ -9,7 +9,11 @@
 import Foundation
 import UIKit
 
-class SelectIcon: UIViewController{
+class SelectIcon: UIViewController, UIScrollViewDelegate{
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     
     @IBOutlet weak var backgroundLogo1:UIView!
     @IBOutlet weak var backgroundLogo2:UIView!
@@ -23,6 +27,8 @@ class SelectIcon: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         constructViews()
+        
+        self.scrollView.delegate = self
     }
     
     /*private func constructViews(){
@@ -33,7 +39,40 @@ class SelectIcon: UIViewController{
         }
     }*/
     
-    private construcViews(){
+    private func constructViews(){
+        let allBackgroundViews: [(UIView, UIView)] = [(self.backgroundLogo1, self.backgroundLogo2), (self.backgroundLogo3, self.backgroundLogo4), (self.backgroundLogo5, self.backgroundLogo6)]
+        let background_width: CGFloat = self.view.frame.width / 3
+        let allBackgroundImage = [] // link to all icon's images
         
+        // Stock the view above the one we are setting up. By default, the first views are set up below the top scroll view
+        var topLeftView: NSLayoutYAxisAnchor = self.scrollView.topAnchor
+        var topRightView: NSLayoutYAxisAnchor = self.scrollView.topAnchor
+        
+        for (left_view, right_view) in allBackgroundViews {
+            
+           
+            
+            // Left view
+            
+            left_view.translatesAutoresizingMaskIntoConstraints = false
+            left_view.widthAnchor.constraint(equalToConstant: background_width).isActive = true
+            left_view.heightAnchor.constraint(equalToConstant:background_width).isActive = true
+            left_view.leftAnchor.constraint(equalTo: self.scrollView.leftAnchor, constant: 20).isActive = true
+            left_view.topAnchor.constraint(equalToSystemSpacingBelow: topLeftView, multiplier: 2).isActive = true
+            
+            
+            
+            
+            // right view
+            right_view.translatesAutoresizingMaskIntoConstraints = false
+            right_view.widthAnchor.constraint(equalToConstant: background_width).isActive = true
+            right_view.heightAnchor.constraint(equalToConstant: background_width).isActive = true
+            right_view.leftAnchor.constraint(equalTo: self.scrollView.rightAnchor, constant: -20).isActive = true
+            right_view.topAnchor.constraint(equalToSystemSpacingBelow: topRightView, multiplier: 2).isActive = true
+            
+            // update the top view
+            topLeftView = left_view
+            topRightView = right_view
+        }
     }
 }
